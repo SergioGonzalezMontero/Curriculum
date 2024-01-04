3b29b998ad9b3b85da301c7fdafc761eeb64921d
SergioGonzalezMontero <155583652+SergioGonzalezMontero@users.noreply.github.com>
2024-01-04 20:22:03 +0100

    Add files via upload

--- recursos/app.js
+++ recursos/app.js
@@ -6,6 +6,8 @@ function mostrarContenido(seccion) {
         if (elemento) {
             elemento.style.display = 'none';
         }
+		var mobileNav = document.getElementById('mobileNav');
+    mobileNav.style.display = 'none';
     }
 
     // Oculta la foto en todas las secciones
@@ -32,4 +34,56 @@ mostrarContenido('contenido');
 document.getElementById('fotoContainer').addEventListener('load', function () {
     // Transición de la imagen al cargarse
     document.getElementById('fotoContainer').style.opacity = '1';
+});
+
+// Función para mostrar/ocultar el menú desplegable
+function toggleMobileMenu() {
+    var mobileNav = document.getElementById('mobileNav');
+    var mobileMenuBtn = document.getElementById('mobileMenuBtn');
+
+    if (window.getComputedStyle(mobileNav).display === 'none') {
+        // Obtener la posición del botón del menú
+        var rect = mobileMenuBtn.getBoundingClientRect();
+
+        // Establecer la posición del menú justo debajo del botón
+        mobileNav.style.position = 'fixed';
+        mobileNav.style.left = '50%';
+        mobileNav.style.transform = 'translateX(-50%)';
+        mobileNav.style.top = rect.bottom + 'px';
+        mobileNav.style.display = 'block';
+
+        // Actualizar la posición del menú al hacer scroll
+        window.addEventListener('scroll', function () {
+            var rect = mobileMenuBtn.getBoundingClientRect();
+            mobileNav.style.left = '50%';
+            mobileNav.style.transform = 'translateX(-50%)';
+            mobileNav.style.top = rect.bottom + 'px';
+        });
+    } else {
+        mobileNav.style.display = 'none';
+    }
+}
+
+// Actualizar la posición del menú al hacer scroll
+window.addEventListener('scroll', function () {
+    var mobileNav = document.getElementById('mobileNav');
+    var mobileMenuBtn = document.getElementById('mobileMenuBtn');
+
+    if (window.getComputedStyle(mobileNav).display !== 'none') {
+        // Obtener la posición actualizada del botón del menú
+        var rect = mobileMenuBtn.getBoundingClientRect();
+
+        // Establecer la posición del menú justo debajo del botón
+        mobileNav.style.left = rect.left + 'px';
+        mobileNav.style.top = rect.bottom + 'px';
+    }
+});
+
+// Agregar evento para cerrar el menú al hacer clic en cualquier lugar fuera del menú
+document.addEventListener('click', function (event) {
+    var mobileNav = document.getElementById('mobileNav');
+    var mobileMenuBtn = document.getElementById('mobileMenuBtn');
+    if (!mobileNav.contains(event.target) && event.target !== mobileMenuBtn) {
+        mobileNav.style.display = 'none';
+    }
 });
