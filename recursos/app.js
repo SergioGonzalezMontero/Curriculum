549fddec17925964d3e5385ee5e244e2bc1810b1
Sergio Gonzalez Montero <155583652+SergioGonzalezMontero@users.noreply.github.com>
2024-05-22 21:51:29 +0200

    Add files via upload

--- recursos/app.js
+++ recursos/app.js
@@ -1,6 +1,6 @@
 function mostrarContenido(seccion) {
     // Oculta todos los contenidos y la foto
-    var secciones = ['contenido', 'datosPersonales', 'formacionAcademica', 'formacionComplementaria', 'experienciaLaboral', 'datosInteres', 'gitHup'];
+    var secciones = ['contenido', 'datosPersonales', 'formacionAcademica', 'formacionProgramacion' ,'formacionComplementaria', 'experienciaLaboral', 'datosInteres', 'gitHup'];
     for (var i = 0; i < secciones.length; i++) {
         var elemento = document.getElementById(secciones[i]);
         if (elemento) {
@@ -32,4 +32,25 @@ mostrarContenido('contenido');
 document.getElementById('fotoContainer').addEventListener('load', function () {
     // Transición de la imagen al cargarse
     document.getElementById('fotoContainer').style.opacity = '1';
+});
+
+// JavaScript para gestionar el cambio en el menú desplegable
+document.addEventListener('DOMContentLoaded', function () {
+    var sections = document.querySelectorAll('section');
+
+    function mostrarContenido(seccion) {
+        // Oculta todos los contenidos
+        sections.forEach(function (element) {
+            element.style.display = 'none';
+        });
+
+        // Muestra el contenido de la sección seleccionada
+        var contenido = document.querySelector(seccion);
+        if (contenido) {
+            contenido.style.display = 'block';
+        }
+    }
+
+    // Mostrar el mensaje de bienvenida al cargar la página
+    mostrarContenido('#contenido');
 });
